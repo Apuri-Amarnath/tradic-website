73ba3a474124b3a10c818f9773a1ac770b8c2400
Apuri-Amarnath <apuriamarnath123@gmail.com>
2024-08-07 15:01:04 +0530

    hompage modifications

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import ContactBanner from '../assets/images/contact.jpg'
+import {Box, Container} from "@mui/material";
 
 const Contact = () => {
     return (
-        <div>
-            
-        </div>
+        <Container maxWidth={false}>
+            <Box sx={{
+                backgroundImage: `url(${ContactBanner})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                height: {
+                    sx:'35vh',
+                    md:'30vh',
+                    xs:'20vh',
+                    lg:'45vh',
+                },
+                width: '100%',
+                display: 'flex',
+                backgroundPosition: 'center',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+            </Box>
+        </Container>
     );
 };
 
